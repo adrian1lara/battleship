92aae0ebb98334e4f6ae9593e1f46954becf641d
adrian1lara <adrianlara203@gmail.com>
2023-09-14 23:30:48 -0500

    initial webpack config and test

---
+++ webpack.test.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      // Add other loaders and rules for different file types as needed.
+    ],
+  },
+};
